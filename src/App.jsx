96e8a016d1be0431903b466f544fd03751f659ce
Dvdrepetto <davidmrepetto@gmail.com>
2025-03-05 12:47:19 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,30 @@ import GreyTheme from "./templates/GreyTheme";
 import LightThemeTemplate from "./templates/LightTheme";
 import Theme from "./templates/Theme";
 
+import React from "react";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
+import PortfolioPage from "./pages/PortfolioPage";
+
+
 function App() {
   return (
     <>
-      <BoldTheme />
+      <h1>APP component</h1>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} /> 
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/portfolios" element={<PortfolioPage />} />
+        <Route path="*" element={ <ErrorPage /> } /> 
+      </Routes>
     </>
   );
 }
