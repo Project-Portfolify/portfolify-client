a5bcd12636d054857d9272963f39ff5fb8920599
Dvdrepetto <davidmrepetto@gmail.com>
2025-03-05 12:47:19 +0200

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+
   </StrictMode>,
 )
