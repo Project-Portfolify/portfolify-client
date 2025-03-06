bdbdc429d53fc1036fcc1487d403640b91647bca
uzma shaik <uzma97910@gmail.com>
2025-03-06 23:47:04 +0100

    feat(style):portfolio cards design

--- src/components/PortfolioList.jsx
+++ src/components/PortfolioList.jsx
@@ -1,24 +1,59 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import DarkTheme from "../assets/DarkTheme.png";
+import GreyTheme from "../assets/GreyTheme.png";
+import LightTheme from "../assets/LightTheme.png";
+import AtomTheme from "../assets/AtomTheme.png";
+import BoldTheme from "../assets/BoldTheme.png";
+import { Templates } from "../constants";
+import { useNavigate } from "react-router-dom";
+
+const templateList = [
+  {
+    templateId: Templates.AtomTheme,
+    image: AtomTheme,
+  },
+  {
+    templateId: Templates.LightTheme,
+    image: LightTheme,
+  },
+  {
+    templateId: Templates.BoldTheme,
+    image: BoldTheme,
+  },
+  {
+    templateId: Templates.GreyTheme,
+    image: GreyTheme,
+  },
+  {
+    templateId: Templates.DarkTheme,
+    image: DarkTheme,
+  },
+];
 
 const PortfolioList = () => {
   const carouselRef = useRef(null);
-
+  const navigate = useNavigate();
   const scrollLeft = () => {
     if (carouselRef.current) {
-      carouselRef.current.scrollBy({ left: -300, behavior: 'smooth' });
+      carouselRef.current.scrollBy({ left: -300, behavior: "smooth" });
     }
   };
 
   const scrollRight = () => {
     if (carouselRef.current) {
-      carouselRef.current.scrollBy({ left: 300, behavior: 'smooth' });
+      carouselRef.current.scrollBy({ left: 300, behavior: "smooth" });
     }
   };
 
+  const handlePreview = (templateId) => {
+    navigate(`/preview/${templateId}`);
+  };
+
   return (
-    <section id="portfolios" className="w-full h-auto px-4 py-2 overflow-hidden bg-gradient-to-r "
->
+    <section
+      id="portfolios"
+      className="w-full h-auto px-4 py-2 overflow-hidden bg-gradient-to-r "
+    >
       <div className="relative">
         {/* Scroll to the Left Button */}
         <button
@@ -31,154 +66,49 @@ const PortfolioList = () => {
         {/* Carousel Container */}
         <div
           ref={carouselRef}
-          className="flex flex-nowrap gap-10 overflow-x-auto scroll-smooth pb-4"
+          className="flex flex-nowrap gap-10 overflow-x-auto scroll-smooth pb-4 m-20"
         >
           {/* Card Template 1 */}
-          <div className="min-w-[300px] flex-shrink-0">
-            <div
-              className="w-[300px] h-[400px] overflow-hidden bg-white text-slate-500 shadow-2xl shadow-slate-200 mt-4 border-2 border-gray-500 rounded-xl relative"
-              style={{
-                backgroundImage: `url(${DarkTheme})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="relative p-6">
-                <figure className="relative z-10">
-                  <blockquote className="p-6 text-lg leading-relaxed">
-                  </blockquote>
-                </figure>
-              </div>
-            </div>
-          </div>
+          {templateList.map((obj, i) => {
+            return (
+              <div
+                key={i}
+                className="flex-shrink-0 m-4 relative overflow-hidden bg-white rounded-lg max-w-xs shadow-lg h-auto"
+              >
+                {/* Image on Top */}
+                <img
+                  className="w-100 h-90 object-cover"
+                  src={obj.image}
+                  alt="darkTheme"
+                />
 
-          {/* Card Template 2 */}
-          <div className="min-w-[300px] flex-shrink-0">
-            <div
-              className="w-[300px] h-[400px] overflow-hidden bg-white text-slate-500 shadow-2xl shadow-slate-200 mt-4 border-2 border-gray-500 rounded-xl relative"
-              style={{
-                backgroundImage: `url(${DarkTheme})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="relative p-6">
-                <figure className="relative z-10">
-                  <blockquote className="p-6 text-lg leading-relaxed">
-                  </blockquote>
-                </figure>
+                {/* Content & Buttons at Bottom */}
+                <div className="relative text-black px-4 pb-4 mt-2 flex flex-col items-center">
+                  <div className="flex justify-between w-full">
+                    <button
+                      onClick={() => handlePreview(obj.templateId)}
+                      className="w-20 h-8 shadow-sm rounded-lg bg-indigo-600 hover:bg-indigo-800 transition-all duration-300 text-white text-sm"
+                    >
+                      Preview
+                    </button>
+                    <button className="w-20 h-8 shadow-sm rounded-lg bg-indigo-600 hover:bg-indigo-800 transition-all duration-300 text-white text-sm">
+                      Create
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          {/* Card Template 3 */}
-          <div className="min-w-[300px] flex-shrink-0">
-            <div
-              className="w-[300px] h-[400px] overflow-hidden bg-white text-slate-500 shadow-2xl shadow-slate-200 mt-4 border-2 border-gray-500 rounded-xl relative"
-              style={{
-                backgroundImage: `url(${DarkTheme})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="relative p-6">
-                <figure className="relative z-10">
-                  <blockquote className="p-6 text-lg leading-relaxed">
-                  </blockquote>
-                </figure>
-              </div>
-            </div>
-          </div>
-
-          {/* Card Template 4 */}
-          <div className="min-w-[300px] flex-shrink-0">
-            <div
-              className="w-[300px] h-[400px] overflow-hidden bg-white text-slate-500 shadow-2xl shadow-slate-200 mt-4 border-2 border-gray-500 rounded-xl relative"
-              style={{
-                backgroundImage: `url(${DarkTheme})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="relative p-6">
-                <figure className="relative z-10">
-                  <blockquote className="p-6 text-lg leading-relaxed">
-                  </blockquote>
-                </figure>
-              </div>
-            </div>
-          </div>
-          {/* Card Template 5 */}
-          <div className="min-w-[300px] flex-shrink-0">
-            <div
-              className="w-[300px] h-[400px] overflow-hidden bg-white text-slate-500 shadow-2xl shadow-slate-200 mt-4 border-2 border-gray-500 rounded-xl relative"
-              style={{
-                backgroundImage: `url(${DarkTheme})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="relative p-6">
-                <figure className="relative z-10">
-                  <blockquote className="p-6 text-lg leading-relaxed">
-                  </blockquote>
-                </figure>
-              </div>
-            </div>
-          </div>
-          {/* Card Template 6 */}
-          <div className="min-w-[300px] flex-shrink-0">
-            <div
-              className="w-[300px] h-[400px] overflow-hidden bg-white text-slate-500 shadow-2xl shadow-slate-200 mt-4 border-2 border-gray-500 rounded-xl relative"
-              style={{
-                backgroundImage: `url(${DarkTheme})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="relative p-6">
-                <figure className="relative z-10">
-                  <blockquote className="p-6 text-lg leading-relaxed">
-                  </blockquote>
-                </figure>
-              </div>
-            </div>
-          </div>
-          {/* Card Template 7 */}
-          <div className="min-w-[300px] flex-shrink-0">
-            <div
-              className="w-[300px] h-[400px] overflow-hidden bg-white text-slate-500 shadow-2xl shadow-slate-200 mt-4 border-2 border-gray-500 rounded-xl relative"
-              style={{
-                backgroundImage: `url(${DarkTheme})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="relative p-6">
-                <figure className="relative z-10">
-                  <blockquote className="p-6 text-lg leading-relaxed">
-                  </blockquote>
-                </figure>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
-
-        {/* Scroll to the Right Button */}
-        <button
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 bg-gray-400 text-black p-2 rounded-lg"
-          onClick={scrollRight}
-        >
-          &gt;
-        </button>
       </div>
+
+      {/* Scroll to the Right Button */}
+      <button
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 bg-gray-400 text-black p-2 rounded-lg"
+        onClick={scrollRight}
+      >
+        &gt;
+      </button>
     </section>
   );
 };
