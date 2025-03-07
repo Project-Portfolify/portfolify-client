fef81728f5bef981d696db121089f300f8e6db11
uzma shaik <uzma97910@gmail.com>
2025-03-07 11:16:50 +0100

    feat:add prev step button

--- src/components/TemplateSwitch.jsx
+++ src/components/TemplateSwitch.jsx
@@ -1,9 +1,15 @@
 import AtomTheme from "../templates/AtomTheme";
 
-const TemplateSwitch = ({ template, data }) => {
+const TemplateSwitch = ({ templateId, data, onClickPrev }) => {
   return (
     <div>
-      <div className="flex justify-end mb-20">
+      <div className="flex justify-between mb-20">
+        <button
+          className="w-52 h-12 shadow-sm rounded-full bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 text-white text-base font-semibold leading-7"
+          onClick={onClickPrev}
+        >
+          Previous step
+        </button>
         <button className="w-52 h-12 shadow-sm rounded-full bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 text-white text-base font-semibold leading-7">
           Publish
         </button>
