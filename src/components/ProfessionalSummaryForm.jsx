05adc1ac912afb2be2a664cc970a9482654ec867
uzma shaik <uzma97910@gmail.com>
2025-03-09 15:49:52 +0100

    feat:add multiple projects

--- src/components/ProfessionalSummaryForm.jsx
+++ src/components/ProfessionalSummaryForm.jsx
@@ -1,8 +1,6 @@
 import { Asterisk } from "lucide-react";
-import { useForm } from "react-hook-form";
 
-const ProfessionalSummaryForm = ({onClickNext, onClickPrev, form}) => {
-
+const ProfessionalSummaryForm = ({ onClickNext, onClickPrev, form }) => {
   const {
     register,
     handleSubmit,
@@ -29,10 +27,10 @@ const ProfessionalSummaryForm = ({onClickNext, onClickPrev, form}) => {
                 {...register("role", { required: true })}
               />
               {errors.role && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
+                <span className="text-red-600 text-xs">
+                  This field is required
+                </span>
+              )}
             </div>
             <div className="w-full relative">
               <label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
@@ -46,10 +44,10 @@ const ProfessionalSummaryForm = ({onClickNext, onClickPrev, form}) => {
                 {...register("company", { required: true })}
               />
               {errors.company && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
+                <span className="text-red-600 text-xs">
+                  This field is required
+                </span>
+              )}
             </div>
           </div>
           <div className="flex flex-row gap-x-6 mb-6">
@@ -66,10 +64,10 @@ const ProfessionalSummaryForm = ({onClickNext, onClickPrev, form}) => {
                 {...register("roleDescription", { required: true })}
               />
               {errors.roleDescription && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
+                <span className="text-red-600 text-xs">
+                  This field is required
+                </span>
+              )}
             </div>
             <div className="w-full relative">
               <label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
@@ -85,10 +83,10 @@ const ProfessionalSummaryForm = ({onClickNext, onClickPrev, form}) => {
                   {...register("yearFrom", { required: true })}
                 />
                 {errors.duration && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
+                  <span className="text-red-600 text-xs">
+                    This field is required
+                  </span>
+                )}
                 <span className="flex items-center text-gray-600">-</span>
                 <Asterisk size={10} color="red" />
                 <input
@@ -99,19 +97,18 @@ const ProfessionalSummaryForm = ({onClickNext, onClickPrev, form}) => {
                   {...register("yearTo", { required: true })}
                 />
                 {errors.yearTo && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
-            
+                  <span className="text-red-600 text-xs">
+                    This field is required
+                  </span>
+                )}
               </div>
             </div>
           </div>
 
           <div className="flex justify-between w-full mt-6">
-            <button 
-            className="w-52 h-12 shadow-sm rounded-full bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 text-white text-base font-semibold leading-7"
-            onClick={onClickPrev}
+            <button
+              className="w-52 h-12 shadow-sm rounded-full bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 text-white text-base font-semibold leading-7"
+              onClick={onClickPrev}
             >
               Previous step
             </button>
--- src/components/ProjectDetailsForm.jsx
+++ src/components/ProjectDetailsForm.jsx
@@ -1,76 +1,55 @@
-import { Asterisk } from "lucide-react";
+import { useFieldArray } from "react-hook-form";
+import AddProjectForm from "./AddProjectForm";
+import { SquarePlus } from "lucide-react";
+import { useEffect } from "react";
 
 const ProjectDetailsForm = ({ form, onClickSubmit, onClickPrev }) => {
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors },
   } = form;
 
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "projects",
+  });
+
   const handleOnSubmit = (data) => {
     console.log(data);
     onClickSubmit();
   };
+  console.log(errors);
+
+  useEffect(() => {
+    append();
+  }, []);
 
   return (
     <form onSubmit={handleSubmit(handleOnSubmit)}>
       <div className="flex flex-col">
-        <div className="flex flex-row gap-x-6 mb-6">
-          <div className="w-full relative">
-            <label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
-              Title
-              <Asterisk size={10} color="red" />
-            </label>
-            <input
-              type="text"
-              className="block w-full px-5 py-2.5 bg-white leading-7 text-base font-normal shadow-xs text-gray-900 border border-gray-300 rounded-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              {...register("title", { required: true })}
+        {fields.map((_, index) => {
+          return (
+            <AddProjectForm
+              key={index}
+              index={index}
+              register={register}
+              remove={remove}
+              error={errors.projects?.[index]}
             />
-            {errors.title && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
-          </div>
-          <div className="w-full relative">
-            <label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
-              Description
-              <Asterisk size={10} color="red" />
-            </label>
-            <input
-              type="text"
-              className="block w-full px-5 py-2.5 bg-white leading-7 text-base font-normal shadow-xs text-gray-900 border border-gray-300 rounded-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              {...register("description", { required: true })}
-            />
-            {errors.description && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-row gap-x-6 mb-6">
-          <div className="w-100 relative">
-            <label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
-              Project Link
-              <Asterisk size={10} color="red" />
-            </label>
-            <input
-              type="text"
-              className="block w-full px-5 py-2.5 bg-white leading-7 text-base font-normal shadow-xs text-gray-900 border border-gray-300 rounded-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              {...register("link", { required: true })}
-            />
-            {errors.link && (
-              <span className="text-red-600 text-xs">
-                This field is required
-              </span>
-            )}
-          </div>
-          <button className="w-52 h-12 shadow-sm rounded-full bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 text-white text-base font-semibold leading-7">
-            + Add Project
-          </button>
-        </div>
+          );
+        })}
 
+        <div
+          onClick={() => append({ title: "", description: "", link: "" })}
+          className="flex text-gray-500 hover:cursor-pointer  hover:scale-110  w-35 p-2 h-10 rounded-md"
+        >
+          <div className="">
+            <SquarePlus />
+          </div>
+          <div className="text-md ml-2">Add Project</div>
+        </div>
         <div className="flex justify-between w-full mt-6">
           <button
             className="w-52 h-12 shadow-sm rounded-full bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 text-white text-base font-semibold leading-7"
