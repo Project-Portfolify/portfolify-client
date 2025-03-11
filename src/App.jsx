705500e3d4e90aa8612852ed86b62f87db52d20b
uzma shaik <uzma97910@gmail.com>
2025-03-11 13:04:36 +0100

    feat(auth):protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,92 @@
 import { AuthProvider } from "./context/AuthContext";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import React from "react";
+import React, { Children } from "react";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-
 import PortfolioPage from "./pages/PortfolioPage";
 import CreatePortfolio from "./pages/CreatePortfolio";
 import PreviewPage from "./pages/PreviewPage";
-import TemplateSwitch from "./components/TemplateSwitch";
+import PublishedPortfolioPage from "./pages/PublishedPortfolioPage";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/templates/:templateId" element={<CreatePortfolio />} />
-          <Route path="/preview/:templateId" element={<PreviewPage />} />
-          <Route path="/portfolio/:slug" element={<TemplateSwitch />} />
-          <Route path="/portfolios" element={<PortfolioPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AuthProvider>
+              <SignUp />
+            </AuthProvider>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <AuthProvider>
+              <Login />
+            </AuthProvider>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PrivateRoute>
+              <AboutPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/templates/:templateId"
+          element={
+            <PrivateRoute>
+              <CreatePortfolio />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/preview/:templateId"
+          element={
+            <PrivateRoute>
+              <PreviewPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/portfolios"
+          element={
+            <PrivateRoute>
+              <PortfolioPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="/portfolio/:slug" element={<PublishedPortfolioPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </>
   );
 }
 
 export default App;
+
+function PrivateRoute({ children }) {
+  return (
+    <AuthProvider>
+      <Navbar />
+      {children}
+    </AuthProvider>
+  );
+}
