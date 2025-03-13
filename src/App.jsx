606d2606a25d5c60715ac88835455d139f12a41b
Dvdrepetto <davidmrepetto@gmail.com>
2025-03-13 15:21:59 +0200

    fix bug with div in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Routes>
         <Route
           path="/"
